ca5aa226f73307a0181aea586c43537cfc2fb7be
SURYAIDURI <tejs4100@gmail.com>
2022-12-27 12:28:18 +0530

    Lets Get Started with Reacts

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItem from "./Components/ExpenseItem";
+function App(){
+    return(
+        <div>
+            <h2> Let get Started!</h2>
+            <ExpenseItem></ExpenseItem>
+        </div>
+    );
+}
+
+export default App; 
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+    return <div>
+        <h1> Expense Items</h1>
+        <h2> Food  RS 10</h2>
+        <h2>Petrol RS 100</h2>
+        <h2>Movies RS 200</h2>
+    </div>
+        
+     
+}
+export default ExpenseItem;

dd839104559b8c66f2dd5e51f70c476a2c1bf0af
SURYAIDURI <tejs4100@gmail.com>
2022-12-27 18:01:10 +0530

    Passing Data

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,26 @@
-function ExpenseItem() {
-    return <div>
-        <h1> Expense Items</h1>
-        <h2> Food  RS 10</h2>
-        <h2>Petrol RS 100</h2>
-        <h2>Movies RS 200</h2>
-    </div>
-        
-     
+import './ExpenseItem.css';
+
+
+function ExpenseItem(props) {
+
+
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+    const expenseLocationOfExpenditure ='DELHI'
+
+
+
+    return (
+        <div className='expense-item '>
+            <h2>{props.date.toISOString()}</h2>
+            <div className='expense-item__description  '>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'> ${props.amount} </div>
+                <h2>{expenseLocationOfExpenditure}</h2>
+            </div>
+        </div>
+    )
+
 }
-export default ExpenseItem;
+export default ExpenseItem;  

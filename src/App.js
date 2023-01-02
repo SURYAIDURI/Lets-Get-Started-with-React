cddb682293e1ae164b4eac7defae9de65521a768
SURYAIDURI <tejs4100@gmail.com>
2023-01-02 18:52:42 +0530

    Rendering Expenses with performance optimisations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState} from 'react';
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import NewExpense from './Components/NewExpense/NewExpense';
+import Expenses from './Components/Expenses/Expenses';
 
-const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -26,11 +25,19 @@ const App = () => {
     },
   ];
 
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
-  };
+  const App = () => {
 
+  const[expenses, setExpenses] = useState(DUMMY_EXPENSES)
+
+  const addExpenseHandler = (expense) => {
+    // console.log('In App.js');
+    // console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    })
+  
+  }
+}
   // return React.createElement(
   //   'div',
   //   {},
@@ -38,12 +45,12 @@ const App = () => {
   //   React.createElement(Expenses, { items: expenses })
   // );
 
-  return (
+ return(
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
-  );
-}
+  
+ )
 
 export default App;
